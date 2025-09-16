9c0aff52e5ec701f5cfd68d5b0d8995eca456d0a
TdubMorris <95392519+TdubMorris@users.noreply.github.com>
2025-09-15 23:29:51 -0500

    Updated to godot 4.5
    also made various changes to the game scene

---
+++ objects/gui/interface_button.gd
@@ -0,0 +1,41 @@
+class_name InterfaceTouchButton
+extends Control
+
+@export var input_action : StringName
+@export var touchscreen_only := false
+
+var touch_index := 0
+var released := true
+
+func _init():
+	if touchscreen_only and not DisplayServer.is_touchscreen_available():
+		hide()
+
+func press():
+	var event = InputEventAction.new()
+	event.action = input_action
+	event.pressed = true
+	Input.parse_input_event(event)
+	released = false
+
+func release():
+	var event = InputEventAction.new()
+	event.action = input_action
+	event.pressed = false
+	Input.parse_input_event(event)
+	released = true
+
+func is_in(pos : Vector2) -> bool:
+	return ((int(pos.x) in range(position.x, position.x + size.x)) and (int(pos.y) in range(position.y, position.y + size.y)))
+
+func _input(event):
+	if event is InputEventScreenTouch:
+		if event.pressed and is_in(event.position):
+			if released:
+				touch_index = event.index
+			if touch_index == event.index:
+				press()
+			else:
+				release()
+		if touch_index == event.index and not event.pressed:
+			release()
