291fd87e63e60cc6b4e5e5022eaba0d6872550c4
TdubMorris <95392519+TdubMorris@users.noreply.github.com>
2025-12-03 17:45:15 -0600

    bullets stuff

--- objects/entities/bullet.gd
+++ objects/entities/bullet.gd
@@ -1,14 +1,22 @@
 extends Node2D
 class_name Bullet
 
+@onready var raycast = $RayCast2D
 @export var velocity: Vector2
 @export var shooter: Node
 
 func _process(delta):
+	raycast.target_position = to_local(position + velocity * delta)
+	if raycast.is_colliding():
+		collision_check(raycast.get_collider())
 	position += velocity * delta
 
+func collision_check(body : Node2D):
+	if body == shooter:
+		return
+	if body.owner.has_method("hit"):
+		body.owner.hit()
+	queue_free()
+
 func _on_area_2d_body_entered(body):
-	if body != shooter:
-		if body.owner.has_method("hit"):
-			body.owner.hit()
-		queue_free()
+	collision_check(body)
--- objects/entities/player.gd
+++ objects/entities/player.gd
@@ -1,20 +1,37 @@
+@tool
 class_name Player
 extends Racer
 
 @onready var bullet_scene = preload("res://objects/entities/bullet.tscn")
 @onready var barrel = $BulletSpawn
+@onready var cooldownTimer = $Cooldown
+
+@export var color : Color = Color(1, 1, 0, 1):
+	set(value):
+		color = value
+		modulate = value
+
+@export var cooldown : float = 0.25
+
+func _ready():
+	cooldownTimer.wait_time = cooldown
 
 func _physics_process(_delta):
+	if Engine.is_editor_hint():
+		return
 	if not (Input.is_action_pressed("Left") and Input.is_action_pressed("Right")):
 		thrust(500)
 		if Input.is_action_pressed("Left"):
 			steer(-1500)
 		elif Input.is_action_pressed("Right"):
 			steer(1500)
-		if Input.is_action_just_pressed("Shoot"):
+		if Input.is_action_pressed("Shoot"):
 			spawn_bullet()
 
 func spawn_bullet():
+	if not cooldownTimer.is_stopped():
+		return
+	cooldownTimer.start()
 	var bullet = bullet_scene.instantiate() as Bullet
 	bullet.global_transform = barrel.global_transform
 	bullet.velocity = linear_velocity + Vector2(300, 0).rotated(rotation)
